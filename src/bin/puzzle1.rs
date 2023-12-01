641aeb524512e09fafd863495321fc3ac7341b2c
Nihar Gajare <nihargajre@gmail.com>
2023-11-30 21:57:21 -0800

    Puzzle 1

--- src/bin/puzzle1.rs
+++ src/bin/puzzle1.rs
@@ -1,3 +1,75 @@
-fn main() {
+use std::collections::HashMap;
 
+fn main() {
+    // let input = include_str!("../../inputs/puzzle1_sample.txt");
+    let input = include_str!("../../inputs/puzzle1.txt");
+
+    let lines: Vec<&str> = input.lines().collect();
+
+    let sum: u32 = lines
+        .iter()
+        .map(|line| {
+            let numbers: Vec<char> = line.chars().filter(|c| c.is_numeric()).collect();
+
+            let number: u32 = format!("{}{}", numbers.first().unwrap(), numbers.last().unwrap())
+                .parse()
+                .unwrap();
+
+            number
+        })
+        .sum();
+
+    println!("-- Part 1 Answer: {}", sum);
+
+    let entries = [
+        ("one", "1"),
+        ("two", "2"),
+        ("three", "3"),
+        ("four", "4"),
+        ("five", "5"),
+        ("six", "6"),
+        ("seven", "7"),
+        ("eight", "8"),
+        ("nine", "9"),
+    ];
+
+    let sum2: u32 = lines
+        .iter()
+        // .map(|line| pre_process(&line))
+        .map(|line| {
+            let mut indices = entries.iter().flat_map(|e| {
+                line.match_indices(e.0)
+                .chain(line.match_indices(e.1))
+            })
+            .collect::<Vec<_>>();
+
+            indices.sort_by(|e1, e2| e1.0.cmp(&e2.0));
+
+            let first = maybe_word_to_num(indices.first().unwrap().1);
+            let last = maybe_word_to_num(indices.last().unwrap().1);
+
+            let number: u32 = format!("{}{}", first, last)
+                .parse()
+                .unwrap();
+
+            number
+        })
+        .sum();
+
+    println!("-- Part 2 Answer: {}", sum2);
+}
+
+fn maybe_word_to_num(maybe_word: &str) -> &str {
+    let mut map = HashMap::new();
+    map.insert("one", "1");
+    map.insert("two", "2");
+    map.insert("three", "3");
+    map.insert("four", "4");
+    map.insert("five", "5");
+    map.insert("six", "6");
+    map.insert("seven", "7");
+    map.insert("eight", "8");
+    map.insert("nine", "9");
+
+    map.get(maybe_word).unwrap_or(&maybe_word)
 }
